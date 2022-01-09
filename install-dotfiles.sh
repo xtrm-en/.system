ae480632c0f0bf01a49388e5cd41cf82e0d9967a
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-01-09 18:44:47 +0100

    f i n a l l y

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-cd "$(dirname "${BASH_SOURCE}")";
+FULLPATH="$(dirname "$(realpath ${BASH_SOURCE})")"
+cd $FULLPATH
 
 git pull origin main;
 
-function syncAll() {
-    rsync --exclude ".git/" \
-        --exclude "install.sh" \
-        --exclude "README.md" \
-        --exclude "LICENSE" \
-        -avh --no-perms . ~;
+function install() {
+    cp -aRfs $FULLPATH/dotfiles/ ~;
+    cd ~/dotfiles
+    cp -av . ../
+    cd ..
+    rm -rf ~/dotfiles
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    syncAll;
+    install;
 else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/N): " -n 1;
     echo "";
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        syncAll;
+        install;
     fi;
 fi;
 
