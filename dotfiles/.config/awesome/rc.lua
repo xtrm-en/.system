045522a97b6dbf3ff00f03ead11c088b68cf01b9
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-04-06 18:08:05 +0200

    updated

--- dotfiles/.config/awesome/rc.lua
+++ dotfiles/.config/awesome/rc.lua
@@ -14,7 +14,7 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 
 -- Notification library
-local naughty = require("naughty")
+-- local naughty = require("naughty")
 
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
@@ -27,6 +27,7 @@ require("awful.hotkeys_popup.keys")
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
+    local naughty = require("naughty")
     naughty.notify({ preset = naughty.config.presets.critical,
                      title = "Oops, there were errors during startup!",
                      text = awesome.startup_errors })
@@ -38,12 +39,6 @@ do
     awesome.connect_signal("debug::error", function (err)
         -- Make sure we don't go into an endless error loop
         if in_error then return end
-        in_error = true
-
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
     end)
 end
 -- }}}
--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -4,5 +4,5 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar --config=/home/xtrm/.config/polybar/config.ini xtrm &
-echo "Launched Polybar [xtrm]"
+polybar --config=$HOME/.config/polybar/config.ini xtrm &
+echo "Launched Polybar"
