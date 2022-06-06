5fa30909fbf5fa9b5805a8c6486c0895356eb5e7
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-06-06 15:09:47 +0200

    dotfiles/autostart: add support for dex

--- dotfiles/.config/autostart/startup.sh
+++ dotfiles/.config/autostart/startup.sh
@@ -5,6 +5,11 @@ picom & # compositor
 # WM Stuff
 /bin/bash ~/.config/polybar/launch.sh & # b a r
 
+# Autostart '.desktop' files
+dex -a
+
+# Polkit Manager
 lxsession &
 
+# Other utilities
 unclutter --timeout 3 &
